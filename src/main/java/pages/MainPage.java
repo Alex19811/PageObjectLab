7512459879e775e5a3900ab97f52a35bd65928b5
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-05-21 23:47:33 +0300

    Page object pattern 21/05/2019

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class MainPage {
+    //Открываем тестовую страницу
+        driver.get("http://magento.mainacad.com/");
 }
+
+    private static By MainPage = By.cssSelector("http://magento.mainacad.com/";
+    private static By RegistrationPage = By.cssSelector(".skip-account";
+//[title='Register']
+    private static By LoginPage = By.cssSelector("//html[@id='top']//div[@id='header-account']//a[@title='Log In']";
+    private static By MyDashboardPage = By.cssSelector("//html[@id='top']//div[@id='header-account']//a[@title='Log In']";
+
+//    AbstractPage
+//          MainPage
+//          RegistrationPage
+//          LoginPage
+//          MyDashboardPage
+
+
+//TODO: Выбрать континент из выпадающего списка
+         WebElement elementSpisok = driver.findElement(ElementSpisok);
+         Select sel = new Select(elementSpisok);
+         sel.selectByVisibleText("Register")
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,7 +1,59 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Tests {
+import java.sql.Driver;
+
+class main page(){
+    private By login buth= By.xpath ("");
+       }
+public main page (){
+    assert )title="main");
+
+        }
+        public loginPage goto loginPage();
+driver.find Elem (loginButh).clic();
+return new loginPage
 
 
-}
+//public class LoginPage {
+//    By usernameLocator = By.id("username");
+//    By passwordLocator = By.id("passwd");
+//    By loginButtonLocator = By.id("login");
+//
+//    private final WebDriver driver;
+//
+//    public LoginPage(WebDriver driver) {
+//        this.driver = driver;
+//
+//        if (!"Login".equals(driver.getTitle())) {
+//            throw new IllegalStateException("This is not the login page");
+//        }
+//    }
+//
+//    public LoginPage typeUsername(String username) {
+//        driver.findElement(usernameLocator).sendKeys(username);
+//        return this;
+//    }
+//
+//    public LoginPage typePassword(String password) {
+//        driver.findElement(passwordLocator).sendKeys(password);
+//        return this;
+//    }
+//
+//    public HomePage submitLogin() {
+//        driver.findElement(loginButtonLocator).submit();
+//        return new HomePage(driver);
+//    }
+//
+//    public LoginPage submitLoginExpectingFailure() {
+//        driver.findElement(loginButtonLocator).submit();
+//        return new LoginPage(driver);
+//    }
+//
+//    public HomePage loginAs(String username, String password) {
+//        typeUsername(username);
+//        typePassword(password);
+//        return submitLogin();
+//    }
+//}
