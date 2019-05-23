1ec16c9a09648a8bcd9a4f678409df88ef524aeb
Aleksandr <aleksandrburlutskiy@gmail.com>
2019-05-23 22:53:10 +0300

    Page object pattern 23/05/2019

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -2,6 +2,7 @@ package pages;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class RegisterPage extends BasePage {
@@ -11,17 +12,57 @@ public class RegisterPage extends BasePage {
 
     private By userNameInput = By.id("firstname");
     private By lastNameInput = By.id("lastname");
-    private By emailAddressInput = By.id("email_address");
-    private By passworInput = By.id("password");
+    private By addressMailAddInput = By.id("email_address");
+    private By passwordInput = By.id("password");
     private By confirmPasswordInput = By.id("confirmation");
 
-    public boolean isUserNamePrecent(){
+    private By singUpForNewsletterInput = By.id("is_subscribed");
 
+    public boolean isUserNamePrecent() {
         return driver.findElement(userNameInput).isDisplayed();
 
     }
-    public boolean isUserNamePrecent(){
 
-        return driver.findElement(userNameInput).isDisplayed();
+    public boolean isLastNamePrecent() {
+        return driver.findElement(lastNameInput).isDisplayed();
 
     }
+    public boolean isEressmailAddPrecent() {
+        return driver.findElement(addressMailAddInput).isDisplayed();
+
+    }
+    public boolean isPassworPrecent() {
+        return driver.findElement(passwordInput).isDisplayed();
+
+    }
+    public boolean isConfirmPasswordPrecent() {
+        return driver.findElement(confirmPasswordInput).isDisplayed();
+
+    }
+
+//    public static void main(String[] args) {
+//        WebElement elementUserName = driver.findElement(userNameInput);
+//        elementUserName.sendKeys("Alekcandr");
+//
+//        WebElement elementLastName = driver.findElement(lastNameInput);
+//        elementLastName.sendKeys("Burlutskyi");
+//
+//        WebElement elementAddressmailAdd = driver.findElement(addressMailAddInput);
+//        elementAddressmailAdd.sendKeys("Aleksandrburlutskiy@gmail.com");
+//
+//        WebElement elementPassword = driver.findElement(passwordInput);
+//        elementPassword.sendKeys("Aleks123456");
+//
+//        WebElement elementConfirmPassword = driver.findElement(confirmPasswordInput);
+//        elementConfirmPassword.sendKeys("Aleks123456");
+
+    WebElement elementSingUpForNewsletterInput = driver.findElement(singUpForNewsletterInput);
+        elementSingUpForNewsletterInput.click();
+
+
+//            }
+
+
+
+}
+
